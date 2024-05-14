3fd85c33c8e5ef16fd71bb912a8d0288afbe3c39
Collins Kinumbi <kinumbicollins254@gmail.com>
2024-05-14 10:30:47 +0300

    Stats

--- index.js
+++ index.js
@@ -93,5 +93,62 @@ async function onMovieSelect(id) {
     },
   });
 
-  console.log(response.data);
+  // console.log(response.data);
+  const { data } = response;
+  console.log(data);
+  document.querySelector("#summary").innerHTML = movieTemplate(data);
+}
+
+function movieTemplate(movieDetail) {
+  const {
+    Poster: poster,
+    Title: title,
+    Genre: genre,
+    Type: type,
+    Plot: plot,
+    Awards: awards,
+    BoxOffice: boxOffice,
+    Metascore: metascore,
+    imdbRating,
+    imdbVotes,
+  } = movieDetail;
+
+  return `
+   <article class='media'>
+     <figure class="media-left">
+       <p class='image'>
+       <img src='${poster}'/>
+       </p>
+     </figure>
+     <div class='media-content'>
+       <div class='content'>
+         <h1>${title}</h1>
+         <h4>${genre}</h4>
+         <h4>${type}</h4>
+         <p>${plot}</p>
+       </div>
+     </div>
+   </article>
+   <article class='notification is-primary'>
+     <p class='title'>${awards ? awards : "N/A"}</p>
+     <p class='subtitle'>Awards</p>
+   </article>
+   <article class='notification is-primary'>
+     <p class='title'>${boxOffice ? boxOffice : "N/A"}</p>
+     <p class='subtitle'>Box Office</p>
+   </article>
+   <article class='notification is-primary'>
+     <p class='title'>${metascore ? metascore : "N/A"}</p>
+     <p class='subtitle'>Metascore</p>
+   </article>
+   <article class='notification is-primary'>
+     <p class='title'>${imdbRating ? imdbRating : "N/A"}</p>
+     <p class='subtitle'>IMDB Rating</p>
+   </article>
+   <article class='notification is-primary'>
+     <p class='title'>${imdbVotes ? imdbVotes : "N/A"}</p>
+     <p class='subtitle'>IMDB votes</p>
+   </article>
+  
+  `;
 }
