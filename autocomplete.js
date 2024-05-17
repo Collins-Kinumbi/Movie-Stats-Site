63a9b27c8e32487dd0f8f3cf9d17be3381aaab67
Collins Kinumbi <kinumbicollins254@gmail.com>
2024-05-17 09:56:18 +0300

    Displaying multiple autocompletes

---
+++ autocomplete.js
@@ -0,0 +1,60 @@
+function createAutoComplete({ root }) {
+  // console.log(root);
+  root.innerHTML = `
+    <lable><b>Search for a movie</b></lable>
+    <input class='input'/>
+    <div class='dropdown'>
+       <div class='dropdown-menu'>
+         <div class="dropdown-content results">
+       </div>
+    </div>
+  `;
+
+  const input = root.querySelector("input");
+
+  const dropdown = root.querySelector(".dropdown");
+
+  const resultsWrapper = root.querySelector(".results");
+
+  const onInput = async (e) => {
+    const movies = await fetchData(e.target.value);
+
+    if (!movies.length) {
+      dropdown.classList.remove("is-active");
+      return;
+    }
+
+    resultsWrapper.innerHTML = "";
+
+    dropdown.classList.add("is-active");
+
+    movies.forEach((movie) => {
+      const { Poster: poster, Title: title, imdbID: id } = movie;
+
+      const option = document.createElement("a");
+
+      const imgSrc = poster === "N/A" ? "" : poster;
+
+      option.classList.add("dropdown-item");
+
+      option.innerHTML = `
+      <img src='${imgSrc}'/>
+          ${title}
+      `;
+
+      option.addEventListener("click", () => {
+        dropdown.classList.remove("is-active");
+        input.value = title;
+        onMovieSelect(id);
+      });
+
+      resultsWrapper.appendChild(option);
+    });
+  };
+
+  input.addEventListener("input", debounce(onInput, 1000));
+
+  document.addEventListener("click", (e) => {
+    !root.contains(e.target) && dropdown.classList.remove("is-active");
+  });
+}
--- index.js
+++ index.js
@@ -23,66 +23,14 @@ const fetchData = async function (searchTerm) {
   }
 };
 
-const root = document.querySelector(".autocomplete");
-
-root.innerHTML = `
-  <lable><b>Search for a movie</b></lable>
-  <input class='input'/>
-  <div class='dropdown'>
-     <div class='dropdown-menu'>
-       <div class="dropdown-content results">
-     </div>
-  </div>
-
-
-`;
-
-const input = document.querySelector("input");
-
-const dropdown = document.querySelector(".dropdown");
-
-const resultsWrapper = document.querySelector(".results");
-
-const onInput = async (e) => {
-  const movies = await fetchData(e.target.value);
-
-  if (!movies.length) {
-    dropdown.classList.remove("is-active");
-    return;
-  }
-
-  resultsWrapper.innerHTML = "";
-
-  dropdown.classList.add("is-active");
-
-  movies.forEach((movie) => {
-    const { Poster: poster, Title: title, imdbID: id } = movie;
-
-    const option = document.createElement("a");
-
-    const imgSrc = poster === "N/A" ? "" : poster;
-
-    option.classList.add("dropdown-item");
-
-    option.innerHTML = `
-    <img src='${imgSrc}'/>
-        ${title}
-    `;
-
-    option.addEventListener("click", () => {
-      dropdown.classList.remove("is-active");
-      input.value = title;
-      onMovieSelect(id);
-    });
-
-    resultsWrapper.appendChild(option);
-  });
-};
-
-input.addEventListener("input", debounce(onInput, 1000));
-
-document.addEventListener("click", (e) => {
-  !root.contains(e.target) && dropdown.classList.remove("is-active");
+createAutoComplete({
+  root: document.querySelector(".autocomplete"),
+});
+createAutoComplete({
+  root: document.querySelector(".autocomplete-two"),
+});
+createAutoComplete({
+  root: document.querySelector(".autocomplete-three"),
 });
 
 async function onMovieSelect(id) {
@@ -95,7 +43,7 @@ async function onMovieSelect(id) {
 
   // console.log(response.data);
   const { data } = response;
-  console.log(data);
+  // console.log(data);
   document.querySelector("#summary").innerHTML = movieTemplate(data);
 }
 
